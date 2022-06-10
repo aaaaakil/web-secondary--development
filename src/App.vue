3338944858f3f630f32df14845d933ea724c8a4f
王若昕 <wohaokun1112@163.com>
2022-06-10 16:42:40 +0800

    页面

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="infoCard">
-    <div class="card-title" @click="triggerEvent">
-      {{ title }}
+  <div class="bigest">
+    <div class="mask">
+      <div class="left">
+        123
+      </div>
+      <div class="right">
+        <h1>登录注册</h1>
+        <div class="rb">
+          <div>
+            <el-input placeholder="请输入手机号" class="mobile"></el-input>
+          </div>
+          <div class="yzcode">
+            <div>
+              <el-input placeholder="请输入内容" class="mobile">
+                <template slot="append">123456</template>
+              </el-input>
+            </div>
+          </div>
+          <div class="yzcode">
+            <div>
+              <el-input placeholder="请输入内容" class="mobile">
+                <template slot="append"><span class="getyzCode">获取验证码</span></template>
+              </el-input>
+            </div>
+          </div>
+          <div>
+            <input type="checkbox">已阅读并同意<span class="getyzCode1">《隐私政策》《服务协议》</span>
+            <el-button type="primary" class="login">登录/注册</el-button>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="card-desc">
-      {{ desc }}
-    </div>
-    <el-button ghost @click="goToStudy"> 去学习 </el-button>
-    <el-button ghost @click="getData"> 获取数据 </el-button>
   </div>
 </template>
 
@@ -66,7 +89,7 @@ export default {
       alert("动作执行成功！");
     },
     Event_Center_getName() {
-      return "应用二开测试";
+      return "登录页面";
     },
   },
   destroyed() {
@@ -74,3 +97,61 @@ export default {
   },
 };
 </script>
+<style scoped>
+.bigest {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-color: #ccc;
+  /* opacity: 0.8; */
+}
+
+.mask {
+  text-align: center;
+  width: 40%;
+  height: 70%;
+  position: fixed;
+  left: 30%;
+  top: 20%;
+  background-color: #fff;
+  display: flex;
+}
+
+.mobile {
+  width: 80%;
+}
+
+.left {
+  width: 30%;
+  height: 100%;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+}
+
+.right {
+  width: 70%;
+  height: 100%;
+  flex: 1;
+}
+
+.rb {
+  width: 100%;
+  height: 60%;
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+.getyzCode,.getyzCode1{
+  color: skyblue;
+}
+.getyzCode1{
+  font-weight:bolder;
+}
+.login{
+  width: 80%;
+  height: 60px;
+  margin-top: 30px;
+}
+</style>
